95c98f3fcb07c86c62f398c58f29bfdfb1e0e91e
MykolaKurin <nvk1967@gmail.com>
2021-11-02 22:07:07 +0200

    Simplelightbox added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const galeryRef = document.querySelector(".gallery");
+
+
+const createImg = galleryItems.map(image =>
+  `<div class="gallery__item">
+       <a class="gallery__item" href="${image.original}">
+            <img class="gallery__image"
+            src="${image.preview}"
+            alt="${image.description}"
+         />
+    </a>
+  </div>`
+).join("");
+
+galeryRef.insertAdjacentHTML('afterbegin', createImg);
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
 console.log(galleryItems);
