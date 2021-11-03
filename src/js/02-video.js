6ef33de94f36c99bed57f5fd0ad3c9c89d54eff6
MykolaKurin <nvk1967@gmail.com>
2021-11-03 21:41:08 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,30 @@ import Player from '@vimeo/player';
 const iFrame = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-    console.log('played the video');
-});
+    player.on('play', function() {
+        console.log('played the video!');
+    });
 
-player.getVideoTitle().then(function (title) {
-    console.log('title:', title);
-});
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+const onPlay = function (data) {
+        console.log(data.seconds)
+    // data is an object containing properties specific to that event
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds))
+};
+
+player.on('play', onPlay);
+
+
+const currentTime = localStorage.getItem("videoplayer-current-time")
+
+// console.log(currentTime) 
+
+
+player.setCurrentTime(currentTime).catch(function (error) {
+    console.log(error)
+})
 
