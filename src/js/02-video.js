879f54e57b24e249180f56c9c8a79d41822033ce
MykolaKurin <nvk1967@gmail.com>
2021-11-03 23:08:07 +0200

    Task3_update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,6 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+
 const iframeEl = document.querySelector('#vimeo-player');
 const iframePlayer = new Player(iframeEl);
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
@@ -9,6 +10,7 @@ iframePlayer.on('timeupdate', throttle(onTimeUpdate, 1000));
 function onTimeUpdate({ seconds }) {
   localStorage.setItem(LOCALSTORAGE_KEY, seconds);
 }
+
 function setTime() {
   const time = localStorage.getItem(LOCALSTORAGE_KEY);
   if (time) {
